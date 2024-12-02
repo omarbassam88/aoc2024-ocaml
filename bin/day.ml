9b43fb0a28f4b86aae46b214c67ad00948a5cfb8
Omar Bassam <omar.bassam88@gmail.com>
2024-12-02 19:56:01 +0200

    day 02

--- bin/day.ml
+++ bin/day.ml
@@ -3,12 +3,13 @@ module type Day = sig
   val part_two : string -> int
 end
 
-let modules = [ ("01", (module Aoc.Day01 : Day)) ]
+let modules =
+  [ ("01", (module Aoc.Day01 : Day)); ("02", (module Aoc.Day02 : Day)) ]
 
 let () =
   let d = Array.get Sys.argv 1 in
-  let input = Aoc.Utils.read_file (Fmt.str "input/day%s.txt" d) in
-  let (module M) = List.assoc d modules in
+  let input = Aoc.Utils.read_file (Fmt.str "input/day%s.txt" d)
+  and (module M) = List.assoc d modules in
   Fmt.pr "Day %s\n" d;
   Fmt.pr "Part 1: %d\n" (M.part_one input);
   Fmt.pr "Part 2: %d\n" (M.part_two input)
---
+++ lib/day02.ml
@@ -0,0 +1,42 @@
+let parse_line line = String.split_on_char ' ' line |> List.map int_of_string
+
+let parse_input input =
+  Utils.lines input |> List.filter (( <> ) "") |> List.map parse_line
+
+let rec calculate_distances = function
+  | [ _ ] | [] -> []
+  | x :: y :: xs -> (y - x) :: calculate_distances (y :: xs)
+
+let within_range num = abs num >= 1 && abs num <= 3
+
+let all_increasing report =
+  report |> calculate_distances
+  |> List.for_all (fun d -> d > 0 && within_range d)
+
+let all_decreasing report =
+  report |> calculate_distances
+  |> List.for_all (fun d -> d < 0 && within_range d)
+
+let remove_from_list lst index = List.filteri (fun i _ -> i <> index) lst
+let is_safe report = all_increasing report || all_decreasing report
+let part_one input = parse_input input |> List.filter is_safe |> List.length
+
+let part_two input =
+  let is_safe_dampened report =
+    is_safe report
+    || List.filteri
+         (fun index _ -> is_safe (remove_from_list report index))
+         report
+       |> List.length |> ( >= ) 1
+  in
+  parse_input input |> List.filter is_safe_dampened |> List.length
+
+let test_input = {|7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9|}
+
+let%test "Part 1: " = part_one test_input = 2
+let%test "Part 2: " = part_two test_input = 4
